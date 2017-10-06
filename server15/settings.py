41014ce8891c7db5f289b0bd352aa9c390e05fd7
Utkal97 <utkal1997@live.in>
2017-10-06 20:05:03 +0530

    Update settings.py

--- server15/settings.py
+++ server15/settings.py
@@ -133,8 +133,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "static/"),
 ]
 
 MEDIA_URL= '/media/'
-MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT= os.path.join(BASE_DIR, 'media/')
