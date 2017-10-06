f87709be7584910e0f560213dc94013cddaafb8a
Utkal97 <utkal1997@live.in>
2017-10-06 23:03:56 +0530

    Update views.py

--- data/views.py
+++ data/views.py
@@ -9,6 +9,10 @@ from rest_framework.response import Response
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
 from data.models import Farmer, Farm, House, Farmpoints, Crop, Wells, Members,PublicPlaces
-from .serializers import HouseSerializer, MembersSerializer, PublicPlacesSerializer, WellsSerializer, FarmSerializer, CropSerializer, FarmerSerializer, FarmpointsSerializer,AudiosSerializer
+from .serializers import HouseSerializer, MembersSerializer, PublicPlacesSerializer, WellsSerializer, FarmSerializer, CropSerializer, FarmerSerializer, FarmpointsSerializer
 
 # Create your views here.
+@detail_route(renderer_classes=(renderers.StaticHTMLRenderer,))
+@csrf_exempt
+def farmerlist(request):
+  if request.method== 'POST'
