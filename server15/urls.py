0e49b87aa38f7065becfc40b1c5d1f0ef5243bac
Utkal97 <utkal1997@live.in>
2017-10-06 18:56:43 +0530

    Update urls.py

--- server15/urls.py
+++ server15/urls.py
@@ -24,6 +24,5 @@ from data.models import *
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
