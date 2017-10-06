482c1a200f6b10219552d587c9c604bb4fb76c21
Utkal97 <utkal1997@live.in>
2017-10-07 00:13:22 +0530

    Update views.py

--- data/views.py
+++ data/views.py
@@ -12,49 +12,46 @@ from .serializers import *
 # Create your views here.
 
 def farmerlist(request):
-  if request.method == 'POST':
     farmer=Farmer.objects.all()
     farmer=FarmerSerializer(farmer,many=True)
     return JSONResponse(farmer.farmer, status=201,safe=False)
   
 def farmlist(request):
-  if request.method == 'POST':
     farm=Farm.objects.all()
     farm=FarmSerializer(farm, many=True)
     return JSONResponse(farm.farm,status=201,safe=False)
 
 def houselist(request):
-  if request.method =='POST':
     house=House.objects.all()
     house=HouseSerializer(house, many=True)
     return JSONResponse(house.house,status=201,safe=False)
   
 def publicplaceslist(request):
-  if request.method == 'POST':
+#  if request.method == 'POST':
     publicplaces=PublicPlaces.objects.all()
     publicplaces=PublicPlacesSerializer(publicplaces, many=True)
     return JSONResponse(publicplaces.publicplaces, status=201, safe=False)
   
 def croplist(request):
-  if request.method =='POST':
+ # if request.method =='POST':
     crop=Crop.objects.all()
     crop=CropSerializer(crop,many=True)
     return JSONResponse(crop.crop, status=201, safe=False)
   
 def memberslist(request):
-  if request.method == 'POST':
+ # if request.method == 'POST':
     members=Members.objects.all()
     members=MembersSerializer(members, many=True)
     return JSONResponse(members.members, status=201, safe=False)
   
 def farmpointslist(request):
-  if request.method == 'POST':
+  #if request.method == 'POST':
     farmpoints= Farmpoints.objects.all()
     farmpoints= FarmpointsSerializer(farmpoints, many=True)
     return JSONResponse(farmpoints.farmpoints, status=201, safe=False)
   
 def wellslist(request):
-  if request.method == 'POST':
+#  if request.method == 'POST':
     wells= Wells.objects.all()
     wells= WellsSerializer(wells, many=True)
     return JSONResponse(wells.wells, status=201, safe=False)
