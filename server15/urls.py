2e809ae76572bfa84196e90b19d2da1c95908a34
Utkal97 <utkal1997@live.in>
2017-10-06 19:38:10 +0530

    Update urls.py

--- server15/urls.py
+++ server15/urls.py
@@ -27,4 +27,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
