3de2a8061a1eba3d492bf8fc3f6e169fdc4e75a4
Utkal97 <utkal1997@live.in>
2017-10-06 20:10:33 +0530

    Update settings.py

--- server15/settings.py
+++ server15/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.gis',
     'data',
     'rest_framework',
+    'rest_framework_gis',
 ]
 
 REST_FRAMEWORK = {
