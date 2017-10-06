af15c73de000c8bb97454cc882379c99a081f40d
Utkal97 <utkal1997@live.in>
2017-10-06 21:51:33 +0530

    Update views.py

--- data/views.py
+++ data/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework import permissions
+from rest_framework import renderers
+from rest_framework import viewsets
+from rest_framework.decorators import detail_route
+from rest_framework.response import Response
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from data.models import Farmer, Farm, House, Farmpoints, Crop, Wells, Members,PublicPlaces
+from .serializers import HouseSerializer, MembersSerializer, PublicPlacesSerializer, WellsSerializer, FarmSerializer, CropSerializer, FarmerSerializer, FarmpointsSerializer,AudiosSerializer
 
 # Create your views here.
