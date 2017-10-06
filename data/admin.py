87edad154e854562eec2f924f3121da76283a635
Utkal97 <utkal1997@live.in>
2017-10-06 11:38:45 +0530

    Update admin.py

--- data/admin.py
+++ data/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from .models import *
+
+# Register your models here.
+
 admin.site.register(Farmer)
-admin.site.register(House)
+admin.site.register(House,admin.GeoModelAdmin)
 admin.site.register(Members)
 admin.site.register(PublicPlaces)
 admin.site.register(Farm)
 admin.site.register(Crop)
 admin.site.register(Wells)
 admin.site.register(Farmpoints)
-# Register your models here.
