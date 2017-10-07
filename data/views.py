3a61bfd458a690f70f4bf2223cc66023381eaad1
Utkal97 <utkal1997@live.in>
2017-10-07 09:48:20 +0530

    Update views.py

--- data/views.py
+++ data/views.py
@@ -14,44 +14,44 @@ from .serializers import *
 def farmerlist(request):
     farmer=Farmer.objects.all()
     farmer=FarmerSerializer(farmer,many=True)
-    return JSONResponse(farmer.data, status=201,safe=False)
+    return JsonResponse(farmer.data, status=201,safe=False)
   
 def farmlist(request):
     farm=Farm.objects.all()
     farm=FarmSerializer(farm, many=True)
-    return JSONResponse(farm.data,status=201,safe=False)
+    return JsonResponse(farm.data,status=201,safe=False)
 
 def houselist(request):
     house=House.objects.all()
     house=HouseSerializer(house, many=True)
-    return JSONResponse(house.data,status=201,safe=False)
+    return JsonResponse(house.data,status=201,safe=False)
   
 def publicplaceslist(request):
 #  if request.method == 'POST':
     publicplaces=PublicPlaces.objects.all()
     publicplaces=PublicPlacesSerializer(publicplaces, many=True)
-    return JSONResponse(publicplaces.data, status=201, safe=False)
+    return JsonResponse(publicplaces.data, status=201, safe=False)
   
 def croplist(request):
  # if request.method =='POST':
     crop=Crop.objects.all()
     crop=CropSerializer(crop,many=True)
-    return JSONResponse(crop.data, status=201, safe=False)
+    return JsonResponse(crop.data, status=201, safe=False)
   
 def memberslist(request):
  # if request.method == 'POST':
     members=Members.objects.all()
     members=MembersSerializer(members, many=True)
-    return JSONResponse(members.data, status=201, safe=False)
+    return JsonResponse(members.data, status=201, safe=False)
   
 def farmpointslist(request):
   #if request.method == 'POST':
     farmpoints= Farmpoints.objects.all()
     farmpoints= FarmpointsSerializer(farmpoints, many=True)
-    return JSONResponse(farmpoints.data, status=201, safe=False)
+    return JsonResponse(farmpoints.data, status=201, safe=False)
   
 def wellslist(request):
 #  if request.method == 'POST':
     wells= Wells.objects.all()
     wells= WellsSerializer(wells, many=True)
-    return JSONResponse(wells.data, status=201, safe=False)
+    return JsonResponse(wells.data, status=201, safe=False)
