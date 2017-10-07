2cf647422f1ed39108d90bb31241ed7c2bc62aad
Utkal97 <utkal1997@live.in>
2017-10-07 10:54:01 +0530

    Update views.py

--- data/views.py
+++ data/views.py
@@ -11,45 +11,68 @@ from .serializers import *
 
 # Create your views here.
 
+@detail_route(renderer_classes=(renderers.StaticHTMLRenderer,))
+@csrf_exempt
 def farmerlist(request):
     farmer=Farmer.objects.all()
     farmer=FarmerSerializer(farmer,many=True)
     return JsonResponse(farmer.data, status=201,safe=False)
   
+ 
+@detail_route(renderer_classes=(renderers.StaticHTMLRenderer,))
+@csrf_exempt
 def farmlist(request):
     farm=Farm.objects.all()
     farm=FarmSerializer(farm, many=True)
     return JsonResponse(farm.data,status=201,safe=False)
 
+
+@detail_route(renderer_classes=(renderers.StaticHTMLRenderer,))
+@csrf_exempt
 def houselist(request):
     house=House.objects.all()
     house=HouseSerializer(house, many=True)
     return JsonResponse(house.data,status=201,safe=False)
   
+
+@detail_route(renderer_classes=(renderers.StaticHTMLRenderer,))
+@csrf_exempt
 def publicplaceslist(request):
 #  if request.method == 'POST':
     publicplaces=PublicPlaces.objects.all()
     publicplaces=PublicPlacesSerializer(publicplaces, many=True)
     return JsonResponse(publicplaces.data, status=201, safe=False)
   
+
+@detail_route(renderer_classes=(renderers.StaticHTMLRenderer,))
+@csrf_exempt
 def croplist(request):
  # if request.method =='POST':
     crop=Crop.objects.all()
     crop=CropSerializer(crop,many=True)
     return JsonResponse(crop.data, status=201, safe=False)
-  
+
+
+@detail_route(renderer_classes=(renderers.StaticHTMLRenderer,))
+@csrf_exempt
 def memberslist(request):
  # if request.method == 'POST':
     members=Members.objects.all()
     members=MembersSerializer(members, many=True)
     return JsonResponse(members.data, status=201, safe=False)
-  
+
+
+@detail_route(renderer_classes=(renderers.StaticHTMLRenderer,))
+@csrf_exempt
 def farmpointslist(request):
   #if request.method == 'POST':
     farmpoints= Farmpoints.objects.all()
     farmpoints= FarmpointsSerializer(farmpoints, many=True)
     return JsonResponse(farmpoints.data, status=201, safe=False)
-  
+
+
+@detail_route(renderer_classes=(renderers.StaticHTMLRenderer,))
+@csrf_exempt
 def wellslist(request):
 #  if request.method == 'POST':
     wells= Wells.objects.all()
